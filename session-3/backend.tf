57a0bc6c7b5f30f91d651d7bf4d9a72952e173be
iriska09 <kudaibergenovayryska09@gmail.com>
2024-05-15 23:20:59 -0500

    push

---
+++ session-3/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-session-backend-yrys"
+    key = "session-3/terraform.tfstate"      //Path to your Remote Backend File (terraform.tfstate)
+    region = "us-west-2"
+  }
+}
---
+++ session-3/providers.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "us-west-2"  // Oregon
+}
+
+// Provider 
+#    - Plugin
+#    - Remote System such as AWS, Azure, Google, etc 
+#    - You must declare provider before you create any Terraform configuration files 
